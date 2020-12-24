9dc622136301b17d81fa09bc2bfae81dffa3a937
Ayush Saini <ayushsainiiitb00@gmail.com>
2020-12-24 20:46:27 +0530

    mainApp

---
+++ mainApp.js
@@ -0,0 +1,32 @@
+const projects = [
+    {
+        index: 1,
+        name : "Binary2Decimal",
+        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/Binary2Decimal"
+    }
+];
+
+const list = document.querySelector('.list');
+
+projects.forEach(({name, code, index}) => {
+
+    const listItem = document.createElement('li');
+
+    listItem.innerHTML = `
+        <span class="project-number">${index}</span>
+        <a href="/${name}/index.html" class="project-name">
+            ${projectNameFormater(name)}
+        </a>
+        <a href=${code} class="container-links">
+            Code
+        </a>
+    `;
+    list.appendChild(listItem);    
+});
+
+function projectNameFormater(name){
+    return name
+		.split('-')
+		.map(word => word[0].toUpperCase() + word.slice(1))
+		.join(' ');
+}
