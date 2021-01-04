6faa2baf4e27276d910983d39adfefceb1b1b7ff
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-01-04 18:48:54 +0530

    hands centered

--- Analog-clock/app.js
+++ Analog-clock/app.js
@@ -17,9 +17,9 @@ function getAngle(){
 
     //now rotate the hands
     //adding translateX here otherwise only rotate works
-    hourHand.style.transform = `rotate(${hrHandRotation}deg) translateX(-50%)`;
-    minuteHand.style.transform = `rotate(${minHandRotation}deg) translateX(-50%)`;
-    secondHand.style.transform = `rotate(${secHandRotation}deg) translateX(-50%)`;
+    hourHand.style.transform = `translateX(-50%) rotate(${hrHandRotation}deg)`;
+    minuteHand.style.transform = `translateX(-50%) rotate(${minHandRotation}deg)`;
+    secondHand.style.transform = `translateX(-50%) rotate(${secHandRotation}deg)`;
 }
 
 getAngle(); //to start the clock as soon as page loads otherwise it jumps to current time abruptly form 12
