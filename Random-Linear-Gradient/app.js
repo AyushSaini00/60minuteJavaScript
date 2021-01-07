de400d44c4bd40314bbe3ef910b4ff66017842a7
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-01-07 18:59:51 +0530

    issue: maybe displayed and generated value don't match

---
+++ Random-Linear-Gradient/app.js
@@ -0,0 +1,33 @@
+//DOM Elements
+const color1Elem = document.getElementById('color-1');
+const color2Elem = document.getElementById('color-2');
+const degreeRangerElem = document.getElementById('degreeRanger');
+const codeElem = document.querySelector('.code');
+
+//Generate HEX
+function getHEX(){
+    let randomHex = Math.random().toString(16).substr(-6);
+    let hexColor = `#${randomHex}`;
+    return hexColor;
+}
+//Generate angle
+function getAngle(){
+    let angle = Math.floor(Math.random()*360);
+    degreeRangerElem.value = angle;
+    return degreeRangerElem.value;
+}
+//Update the UI
+function updateUI(){
+    color1Elem.innerText = getHEX();
+    color2Elem.innerText = getHEX();
+    codeElem.innerText = `background: linear-gradient(${getAngle()}deg, ${color1Elem.innerText}, ${color2Elem.innerText});`;
+    document.body.style.background = `linear-gradient(${getAngle()}deg, ${color1Elem.innerText}, ${color2Elem.innerText})`;
+}
+
+
+//when user clicks SPACE, then UpdateUI
+function key(event){
+    if(event.code === 'Space')
+        updateUI();
+}
+window.addEventListener('keydown', key);
