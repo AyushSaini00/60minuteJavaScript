3c5c2d38f2681359512c5e1567252f59513c9168
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-01-08 18:42:12 +0530

    removed slider for now

--- Random-Linear-Gradient/app.js
+++ Random-Linear-Gradient/app.js
@@ -1,8 +1,9 @@
 //DOM Elements
 const color1Elem = document.getElementById('color-1');
 const color2Elem = document.getElementById('color-2');
-const degreeRangerElem = document.getElementById('degreeRanger');
 const codeElem = document.querySelector('.code');
+const btn = document.querySelector('.btn');
+const infoElem = document.querySelector('.info');
 
 //Generate HEX
 function getHEX(){
@@ -10,26 +11,26 @@ function getHEX(){
     let hexColor = `#${randomHex}`;
     return hexColor;
 }
-//Generate angle
-function getAngle(){
-    let angle = Math.floor(Math.random()*360);
-    degreeRangerElem.value = angle;
-    return degreeRangerElem.value;
-}
+
 //Update the UI
 function updateUI(){
+    infoElem.remove();
+
     color1Elem.innerText = getHEX();
     color2Elem.innerText = getHEX();
+
     color1Elem.style.background = color1Elem.innerText;
     color2Elem.style.background = color2Elem.innerText;
-    codeElem.innerText = `background: linear-gradient(${getAngle()}deg, ${color1Elem.innerText}, ${color2Elem.innerText});`;
-    document.body.style.background = `linear-gradient(${getAngle()}deg, ${color1Elem.innerText}, ${color2Elem.innerText})`;
+
+    codeElem.innerText = `background: linear-gradient(90deg, ${color1Elem.innerText}, ${color2Elem.innerText});`;
+    document.body.style.background = `linear-gradient(90deg, ${color1Elem.innerText}, ${color2Elem.innerText})`;
 }
 
+//when user clicks SPACE or the arrow, then UpdateUI
 
-//when user clicks SPACE, then UpdateUI
-function key(event){
+window.addEventListener('keydown', event => {
     if(event.code === 'Space')
         updateUI();
-}
-window.addEventListener('keydown', key);
+});
+
+btn.addEventListener('click', updateUI);
