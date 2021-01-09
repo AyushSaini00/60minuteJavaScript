6417101f12bd2d8e0553f3a12e8e555be3fa1b38
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-01-09 07:44:39 +0530

    new project: html canvas

---
+++ HTML-Canvas/app.js
@@ -0,0 +1,59 @@
+const canvas = document.getElementById('canvas');
+
+function getCanvas(){
+
+    if(canvas.getContext){
+        //ctx object will provide the drawing interface
+        const ctx = canvas.getContext('2d');
+
+        //resize the canvas according to dimensions of window
+        resizing();
+
+        //flag variable
+        let painting = false;
+
+        function startDrawing(event){
+            painting = true;
+            draw(event);
+        }
+
+        function stopDrawing(){
+            painting = false;
+            ctx.beginPath();
+        }
+
+        function draw(event){
+            //if not painting , then return
+            if(!painting) return;
+    
+            ctx.lineWidth = 10;
+            ctx.lineCap = 'round';
+            ctx.strokeStyle = 'green';        
+
+            ctx.lineTo(event.clientX, event.clientY);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.moveTo(event.clientX, event.clientY);
+        }
+
+        window.addEventListener('mousedown', startDrawing);
+        window.addEventListener('mouseup', stopDrawing);
+        window.addEventListener('mousemove', draw);
+    } else {
+        console.log(`brower doesn't support canvas`);
+    }
+}
+
+window.addEventListener('load', getCanvas);
+
+function resizing(){
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+}
+window.addEventListener('resize', resizing);
+
+
+
+
+
