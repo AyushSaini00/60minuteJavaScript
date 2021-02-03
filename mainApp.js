f7c4133c668c2cff52766d87e6e9ff0452688b39
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-02-03 12:18:52 +0530

    moved project list to json

--- mainApp.js
+++ mainApp.js
@@ -1,118 +1,30 @@
-const projects = [
-    {
-        index: '01',
-        name : "Binary2Decimal",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/Binary2Decimal"
-    },
-    {
-        index: '02',
-        name : "Weather-app",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/Weather-app"
-    },
-    {
-        index: '03',
-        name : "stopwatch",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/stopwatch"
-    },
-    {
-        index: '04',
-        name : "countdown",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/countdown"
-    },
-    {
-        index: '05',
-        name : "Random-Color-Generator",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/Random-Color-Generator"
-    },
-    {
-        index: '06',
-        name : "Analog-clock",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/Analog-clock"
-    },
-    {
-        index: '07',
-        name : "Tip-Calculator",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/Tip-Calculator"
-    },
-    {
-        index: '08',
-        name : "BMI-Calculator",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/BMI-Calculator"
-    },
-    {
-        index: '09',
-        name : "Speed-Reader",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/Speed-Reader"
-    },
-    {
-        index: '10',
-        name : "Box-Shadow-Generator",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/Box-Shadow-Generator"
-    },
-    {
-        index: '11',
-        name : "Password-Generator",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/Password-Generator"
-    },
-    {
-        index: '12',
-        name : "Random-Linear-Gradient",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/Random-Linear-Gradient"
-    },
-    {
-        index: '13',
-        name : "Quotes-Generator",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/Quotes-Generator"
-    },
-    {
-        index: '14',
-        name : "HTML-Canvas",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/HTML-Canvas"
-    },
-    {
-        index: '15',
-        name : "Age-Calculator",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/Age-Calculator"
-    },
-    {
-        index: '16',
-        name : "Form-Validation",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/Form-Validation"
-    },
-    {
-        index: '17',
-        name : "Todo-List",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/Todo-List"
-    },
-    {
-        index: '18',
-        name : "Calculator",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/Calculator"
-    },
-    {
-        index: '19',
-        name : "Code-Music",
-        code : "https://github.com/AyushSaini00/60minuteJavaScript/tree/main/Code-Music"
-    }
-];
-
 const list = document.querySelector('.list');
 
-projects.forEach(({name, code, index}) => {
+fetch("./projects.json")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        updateUI(data);
+    });
 
-    const listItem = document.createElement('li');
+function updateUI(projects){
+    projects.forEach(({name, code, index}) => {
 
-    listItem.innerHTML = `
-        <span class="project-number">${index}</span>
-        <a href="/${name}/index.html" class="project-name">
-            ${projectNameFormater(name)}
-        </a>
-        <a href=${code} class="container-links">
-            Code
-        </a>
-    `;
-    list.appendChild(listItem);    
-});
+        const listItem = document.createElement('li');
+    
+        listItem.innerHTML = `
+            <span class="project-number">${index}</span>
+            <a href="/${name}/index.html" class="project-name">
+                ${projectNameFormater(name)}
+            </a>
+            <a href=${code} class="container-links">
+                Code
+            </a>
+        `;
+        list.appendChild(listItem);    
+    });
+}
 
 function projectNameFormater(name){
     return name
